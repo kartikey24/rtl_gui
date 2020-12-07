4e098bd7e09e4d18f7108104e4e86bfd5db4bb41
Gupta Kartikey Chandresh <kartikey.gupta24@gmail.com>
2020-12-07 19:18:04 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebInterfaceService } from '../web-interface.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rtl';
+
+  var inp=document.getElementById('in');
+  var rtl=document.getElementById('rtl');
+  var out=document.getElementById('out');
+
+  displayResults(): void{
+  	this.webInterfaceService.getData()
+  }
+
+  onSuccess(): void{
+    alert('Your feedback was submitted successfully :)')
+  }
+
+  onClick(): void {
+    console.log(this.inp.value);
+    this.webInterfaceService.submitData(this.inp.value).subscribe(() => this.onSuccess());
+  }
 }
